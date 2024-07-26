53fac1b7fac484df26a424b58cf385165e84f3d8
Sergey Ukustov <sergey@ukstv.me>
2024-07-26 18:19:38 +0300

    test: vitest

--- src/__tests__/intersection.test.ts
+++ src/__tests__/intersection.test.ts
@@ -1,5 +1,4 @@
-import { test } from "uvu";
-import * as assert from "uvu/assert";
+import { test, expect } from "vitest";
 import * as t from "../struct.js";
 import { numberAsString } from "./number-as-string.js";
 import { assertDecode, assertFailure } from "./assertions.util.js";
@@ -9,31 +8,31 @@ const T = t.intersection([t.type({ a: t.string }), t.type({ b: t.number })]);
 
 test("mergeAll", () => {
   const result = t.mergeAll(undefined, [{ prop1: "b", prop2: 2 }, { prop1: "a" }, { prop2: 1 }]);
-  assert.equal(result, { prop1: "a", prop2: 1 });
+  expect(result).toEqual({ prop1: "a", prop2: 1 });
 });
 
 test("name", () => {
-  assert.equal(T.name, "{a:string}&{b:number}");
+  expect(T.name).toEqual("{a:string}&{b:number}");
   const T2 = t.intersection([t.type({ a: t.string }), t.type({ b: t.number })], "T");
-  assert.equal(T2.name, "T");
+  expect(T2.name).toEqual("T");
 });
 
 test("is", () => {
-  assert.ok(T.is({ a: "a", b: 1 }));
-  assert.not.ok(T.is({}));
-  assert.not.ok(T.is({ a: "a" }));
-  assert.not.ok(T.is({ b: 1 }));
+  expect(T.is({ a: "a", b: 1 })).toBeTruthy();
+  expect(T.is({})).toBeFalsy();
+  expect(T.is({ a: "a" })).toBeFalsy();
+  expect(T.is({ b: 1 })).toBeFalsy();
 
   const T2 = t.intersection([t.type({ a: t.string }), t.type({ b: numberAsString })]);
-  assert.ok(T2.is({ a: "a", b: 1 }));
-  assert.not.ok(T2.is({}));
-  assert.not.ok(T2.is({ a: "a" }));
-  assert.not.ok(T2.is({ b: 1 }));
+  expect(T2.is({ a: "a", b: 1 })).toBeTruthy();
+  expect(T2.is({})).toBeFalsy();
+  expect(T2.is({ a: "a" })).toBeFalsy();
+  expect(T2.is({ b: 1 })).toBeFalsy();
 
   const A = t.exact(t.type({ a: t.string }));
   const B = t.exact(t.type({ b: t.number }));
   const T3 = t.intersection([A, B]);
-  assert.ok(T3.is({ a: "a", b: 1 }));
+  expect(T3.is({ a: "a", b: 1 })).toBeTruthy();
 });
 
 test("decode", () => {
@@ -90,56 +89,53 @@ test("decode", () => {
 });
 
 test("encode", () => {
-  assert.equal(T.encode({ a: "a", b: 1 }), { a: "a", b: 1 });
+  expect(T.encode({ a: "a", b: 1 })).toEqual({ a: "a", b: 1 });
   const T1 = t.intersection([t.type({ a: t.string }), t.type({ b: numberAsString })]);
-  assert.equal(T1.encode({ a: "a", b: 1 }), { a: "a", b: "1" });
+  expect(T1.encode({ a: "a", b: 1 })).toEqual({ a: "a", b: "1" });
   const T2 = t.intersection([t.type({ b: numberAsString }), t.type({ a: t.string })]);
-  assert.equal(T2.encode({ a: "a", b: 1 }), { a: "a", b: "1" });
+  expect(T2.encode({ a: "a", b: 1 })).toEqual({ a: "a", b: "1" });
   const T3 = t.intersection([t.type({ b: numberAsString }), t.type({ a: t.string }), t.type({ c: t.string })]);
-  assert.equal(T3.encode({ a: "a", b: 1, c: "c" }), { a: "a", b: "1", c: "c" });
+  expect(T3.encode({ a: "a", b: 1, c: "c" })).toEqual({ a: "a", b: "1", c: "c" });
   const T4 = t.intersection([t.type({ b: numberAsString }), t.type({ a: t.string }), t.type({ c: numberAsString })]);
-  assert.equal(T4.encode({ a: "a", b: 1, c: 2 }), { a: "a", b: "1", c: "2" });
+  expect(T4.encode({ a: "a", b: 1, c: 2 })).toEqual({ a: "a", b: "1", c: "2" });
   const T5 = t.intersection([t.type({ b: numberAsString }), t.type({})]);
-  assert.equal(T5.encode({ b: 1 }), { b: "1" });
-
+  expect(T5.encode({ b: 1 })).toEqual({ b: "1" });
   const A = t.exact(t.type({ a: t.string }));
   const B = t.exact(t.type({ b: t.number }));
   const T6 = t.intersection([A, B]);
-  assert.equal(T6.encode({ a: "a", b: 1 }), { a: "a", b: 1 });
+  expect(T6.encode({ a: "a", b: 1 })).toEqual({ a: "a", b: 1 });
   const x = { a: "a", b: 1, c: true };
-  assert.equal(T6.encode(x), { a: "a", b: 1 });
+  expect(T6.encode(x)).toEqual({ a: "a", b: 1 });
 });
 
 test("handle zero codecs", () => {
   const T = t.intersection([]);
-  assert.ok(T.is(1));
+  expect(T.is(1)).toBeTruthy();
   assertDecode(T, 1, 1);
-  assert.equal(T.encode("a"), "a");
+  expect(T.encode("a")).toEqual("a");
 });
 
 test("handle one codec", () => {
   const T = t.intersection([t.string]);
-  assert.ok(T.is("a"));
-  assert.not.ok(T.is(1));
+  expect(T.is("a")).toBeTruthy();
+  expect(T.is(1)).toBeFalsy();
   assertDecode(T, "a");
   assertFailure(validate(T, 1), "Invalid value 1 supplied to /(string)/0(string)");
-  assert.equal(T.encode("a"), "a");
+  expect(T.encode("a")).toEqual("a");
 });
 
 test("handle primitives", () => {
   const T1 = t.intersection([t.string, t.string]);
-  assert.ok(T1.is("a"));
-  assert.not.ok(T1.is(1));
+  expect(T1.is("a")).toBeTruthy();
+  expect(T1.is(1)).toBeFalsy();
   assertDecode(T1, "a");
   assertFailure(
     validate(T1, 1),
     "Invalid value 1 supplied to /(string&string)/0(string)",
     "Invalid value 1 supplied to /(string&string)/1(string)",
   );
-  assert.equal(T1.encode("a"), "a");
+  expect(T1.encode("a")).toEqual("a");
   const T2 = t.intersection([numberAsString, numberAsString]);
   assertDecode(T2, "1", 1);
-  assert.equal(T2.encode(1), "1");
+  expect(T2.encode(1)).toEqual("1");
 });
-
-test.run();
--- src/__tests__/record.test.ts
+++ src/__tests__/record.test.ts
@@ -1,135 +1,128 @@
-import { suite, test } from "uvu";
-import * as assert from "uvu/assert";
+import { test, expect, describe } from "vitest";
 import * as t from "../struct.js";
 import { numberAsString } from "./number-as-string.js";
 import { hyphenatedString } from "./hyphenated-string.js";
 import { assertDecode, assertFailure } from "./assertions.util.js";
 import { validate } from "../decoder.js";
 
-const nonEnumerable = suite("non-enumerable domain");
+describe("non-enumerable domain", () => {
+  test("name", () => {
+    const T = t.record(t.string, t.number);
+    expect(T.name).toEqual("{[string]:number}");
+    const T2 = t.record(t.string, t.number, "T");
+    expect(T2.name).toEqual("T");
+  });
 
-nonEnumerable("name", () => {
-  const T = t.record(t.string, t.number);
-  assert.equal(T.name, "{[string]:number}");
-  const T2 = t.record(t.string, t.number, "T");
-  assert.equal(T2.name, "T");
+  test("is", () => {
+    const T = t.record(t.string, t.number);
+    expect(T.is({})).toBeTruthy();
+    expect(T.is({ a: 1 })).toBeTruthy();
+    expect(T.is(new Number())).toBeFalsy();
+    expect(T.is({ a: "a" })).toBeFalsy();
+    expect(T.is(null)).toBeFalsy();
+    expect(T.is([])).toBeFalsy();
+
+    const T1 = t.record(t.string, numberAsString);
+    expect(T1.is({})).toBeTruthy();
+    expect(T1.is({ a: 1 })).toBeTruthy();
+    expect(T1.is({ a: "a" })).toBeFalsy();
+    expect(T1.is(null)).toBeFalsy();
+    expect(T1.is([])).toBeFalsy();
+
+    const T2 = t.record(hyphenatedString, t.number);
+    expect(T2.is({})).toBeTruthy();
+    expect(T2.is({ "a-a": 1 })).toBeTruthy();
+    expect(T2.is({ aa: 1 })).toBeFalsy();
+    expect(T2.is([])).toBeFalsy();
+
+    expect(t.record(t.string, t.unknown).is([])).toBeFalsy();
+    expect(t.record(t.string, t.any).is([])).toBeFalsy();
+  });
+
+  test("decode", () => {
+    const T = t.record(t.string, t.number);
+    assertDecode(T, {});
+    assertDecode(T, { a: 1 });
+    assertFailure(validate(T, new Number()), "Invalid value 0 supplied to /({[string]:number})");
+    assertFailure(validate(T, [1]), "Invalid value [1] supplied to /({[string]:number})");
+    assertFailure(validate(T, 1), "Invalid value 1 supplied to /({[string]:number})");
+    assertFailure(validate(T, { aa: "s" }), 'Invalid value "s" supplied to /({[string]:number})/aa(number)');
+
+    const T2 = t.record(t.string, numberAsString);
+    assertDecode(T2, { a: "1" }, { a: 1 });
+
+    const T3 = t.record(hyphenatedString, t.number);
+    assertDecode(T3, { ab: 1 }, { "a-b": 1 });
+    assertFailure(
+      validate(T3, { a: 1 }),
+      'Invalid value "a" supplied to /({[HyphenatedString]:number})/a(HyphenatedString)',
+    );
+
+    const T4 = t.record(t.string, t.any);
+    assertFailure(validate(T4, [1]), "Invalid value [1] supplied to /({[string]:any})");
+  });
+
+  test("encode", () => {
+    const T = t.record(t.string, t.number);
+    expect(T.encode({ a: 1 })).toEqual({ a: 1 });
+
+    const T2 = t.record(t.string, numberAsString);
+    expect(T2.encode({ a: 1 })).toEqual({ a: "1" });
+
+    const T3 = t.record(hyphenatedString, t.number);
+    expect(T3.encode({ "a-b": 1 })).toEqual({ ab: 1 });
+
+    const T4 = t.record(t.string, t.any);
+    expect(T4.encode([])).toEqual({});
+  });
 });
 
-nonEnumerable("is", () => {
-  const T = t.record(t.string, t.number);
-  assert.ok(T.is({}));
-  assert.ok(T.is({ a: 1 }));
-  assert.not.ok(T.is(new Number()));
-  assert.not.ok(T.is({ a: "a" }));
-  assert.not.ok(T.is(null));
-  assert.not.ok(T.is([]));
+describe("enumerable domain", () => {
+  test("name", () => {
+    const T = t.record(t.literal("a"), t.number);
+    expect(T.name).toEqual('{["a"]:number}');
+    const T2 = t.record(t.literal("a"), t.number, "T");
+    expect(T2.name).toEqual("T");
+  });
 
-  const T1 = t.record(t.string, numberAsString);
-  assert.ok(T1.is({}));
-  assert.ok(T1.is({ a: 1 }));
-  assert.not.ok(T1.is({ a: "a" }));
-  assert.not.ok(T1.is(null));
-  assert.not.ok(T1.is([]));
+  test("is", () => {
+    const T = t.record(t.literal("a"), t.string);
+    expect(T.is({ a: "a" })).toBeTruthy();
+    expect(T.is({ a: "a", b: 1 })).toBeTruthy();
+    expect(T.is({})).toBeFalsy();
+  });
 
-  const T2 = t.record(hyphenatedString, t.number);
-  assert.ok(T2.is({}));
-  assert.ok(T2.is({ "a-a": 1 }));
-  assert.not.ok(T2.is({ aa: 1 }));
-  assert.not.ok(T2.is([]));
+  test("decode", () => {
+    const T = t.record(t.literal("a"), t.string);
+    assertDecode(T, { a: "a" });
+    assertDecode(T, { a: "a", b: 1 }, { a: "a" });
+    assertFailure(validate(T, null), 'Invalid value null supplied to /({["a"]:string})');
+    assertFailure(validate(T, {}), 'Invalid value undefined supplied to /({["a"]:string})/a(string)');
+    assertFailure(validate(T, { a: 1 }), 'Invalid value 1 supplied to /({["a"]:string})/a(string)');
 
-  assert.not.ok(t.record(t.string, t.unknown).is([]));
-  assert.not.ok(t.record(t.string, t.any).is([]));
+    const T2 = t.record(t.keyof({ a: null, b: null }), t.string);
+    assertDecode(T2, { a: "a", b: "b" });
+    assertDecode(T2, { a: "a", b: "b", c: 1 }, { a: "a", b: "b" });
+    assertFailure(
+      validate(T2, {}),
+      'Invalid value undefined supplied to /({["a"|"b"]:string})/a(string)',
+      'Invalid value undefined supplied to /({["a"|"b"]:string})/b(string)',
+    );
+    assertFailure(validate(T2, { a: "a" }), 'Invalid value undefined supplied to /({["a"|"b"]:string})/b(string)');
+    assertFailure(validate(T2, { b: "b" }), 'Invalid value undefined supplied to /({["a"|"b"]:string})/a(string)');
+
+    const T3 = t.intersection([t.record(t.literal("a"), t.string), t.record(t.string, t.unknown)]);
+    assertDecode(T3, { a: "a", b: "b" });
+    assertFailure(
+      validate(T3, { b: "b" }),
+      'Invalid value undefined supplied to /({["a"]:string}&{[string]:unknown})/0({["a"]:string})/a(string)',
+    );
+  });
+
+  test("encode", () => {
+    const T = t.record(t.literal("a"), t.number);
+    expect(T.encode({ a: 1 })).toEqual({ a: 1 });
+    const T2 = t.record(t.literal("a"), numberAsString);
+    expect(T2.encode({ a: 1 })).toEqual({ a: "1" });
+  });
 });
-
-nonEnumerable("decode", () => {
-  const T = t.record(t.string, t.number);
-  assertDecode(T, {});
-  assertDecode(T, { a: 1 });
-  assertFailure(validate(T, new Number()), "Invalid value 0 supplied to /({[string]:number})");
-  assertFailure(validate(T, [1]), "Invalid value [1] supplied to /({[string]:number})");
-  assertFailure(validate(T, 1), "Invalid value 1 supplied to /({[string]:number})");
-  assertFailure(validate(T, { aa: "s" }), 'Invalid value "s" supplied to /({[string]:number})/aa(number)');
-
-  const T2 = t.record(t.string, numberAsString);
-  assertDecode(T2, { a: "1" }, { a: 1 });
-
-  const T3 = t.record(hyphenatedString, t.number);
-  assertDecode(T3, { ab: 1 }, { "a-b": 1 });
-  assertFailure(
-    validate(T3, { a: 1 }),
-    'Invalid value "a" supplied to /({[HyphenatedString]:number})/a(HyphenatedString)',
-  );
-
-  const T4 = t.record(t.string, t.any);
-  assertFailure(validate(T4, [1]), "Invalid value [1] supplied to /({[string]:any})");
-});
-
-nonEnumerable("encode", () => {
-  const T = t.record(t.string, t.number);
-  assert.equal(T.encode({ a: 1 }), { a: 1 });
-
-  const T2 = t.record(t.string, numberAsString);
-  assert.equal(T2.encode({ a: 1 }), { a: "1" });
-
-  const T3 = t.record(hyphenatedString, t.number);
-  assert.equal(T3.encode({ "a-b": 1 }), { ab: 1 });
-
-  const T4 = t.record(t.string, t.any);
-  assert.equal(T4.encode([]), {});
-});
-
-nonEnumerable.run();
-
-const enumerable = suite("enumerable domain");
-
-enumerable("name", () => {
-  const T = t.record(t.literal("a"), t.number);
-  assert.equal(T.name, '{["a"]:number}');
-  const T2 = t.record(t.literal("a"), t.number, "T");
-  assert.equal(T2.name, "T");
-});
-
-enumerable("is", () => {
-  const T = t.record(t.literal("a"), t.string);
-  assert.ok(T.is({ a: "a" }));
-  assert.ok(T.is({ a: "a", b: 1 }));
-  assert.not.ok(T.is({}));
-});
-
-enumerable("decode", () => {
-  const T = t.record(t.literal("a"), t.string);
-  assertDecode(T, { a: "a" });
-  assertDecode(T, { a: "a", b: 1 }, { a: "a" });
-  assertFailure(validate(T, null), 'Invalid value null supplied to /({["a"]:string})');
-  assertFailure(validate(T, {}), 'Invalid value undefined supplied to /({["a"]:string})/a(string)');
-  assertFailure(validate(T, { a: 1 }), 'Invalid value 1 supplied to /({["a"]:string})/a(string)');
-
-  const T2 = t.record(t.keyof({ a: null, b: null }), t.string);
-  assertDecode(T2, { a: "a", b: "b" });
-  assertDecode(T2, { a: "a", b: "b", c: 1 }, { a: "a", b: "b" });
-  assertFailure(
-    validate(T2, {}),
-    'Invalid value undefined supplied to /({["a"|"b"]:string})/a(string)',
-    'Invalid value undefined supplied to /({["a"|"b"]:string})/b(string)',
-  );
-  assertFailure(validate(T2, { a: "a" }), 'Invalid value undefined supplied to /({["a"|"b"]:string})/b(string)');
-  assertFailure(validate(T2, { b: "b" }), 'Invalid value undefined supplied to /({["a"|"b"]:string})/a(string)');
-
-  const T3 = t.intersection([t.record(t.literal("a"), t.string), t.record(t.string, t.unknown)]);
-  assertDecode(T3, { a: "a", b: "b" });
-  assertFailure(
-    validate(T3, { b: "b" }),
-    'Invalid value undefined supplied to /({["a"]:string}&{[string]:unknown})/0({["a"]:string})/a(string)',
-  );
-});
-
-enumerable("encode", () => {
-  const T = t.record(t.literal("a"), t.number);
-  assert.equal(T.encode({ a: 1 }), { a: 1 });
-  const T2 = t.record(t.literal("a"), numberAsString);
-  assert.equal(T2.encode({ a: 1 }), { a: "1" });
-});
-
-enumerable.run();
-
-test.run();
